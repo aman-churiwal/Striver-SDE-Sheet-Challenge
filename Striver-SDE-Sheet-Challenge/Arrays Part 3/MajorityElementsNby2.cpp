bb50df11d19dbab078f5a123a079e3c4f812ec21
Amankumar Churiwal <88301170+aman-churiwal@users.noreply.github.com>
2023-06-07 18:28:10 +0530

    Solved a few questions from multiple topics

---
+++ Striver-SDE-Sheet-Challenge/Arrays Part 3/MajorityElementsNby2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+#include <bits/stdc++.h> 
+int findMajorityElement(int arr[], int n) {
+	int majEle = nums[0], votes = 1;
+    
+    for(int i=1; i<n; i++){
+        if(arr[i] == majEle) votes++;
+        else votes--;
+        
+        if(votes==0){
+            majEle = arr[i];
+            votes = 1;
+        }
+    }
+    
+    votes = 0;
+    for(int i=0; i<n; i++){
+        if(arr[i] == majEle) votes++;
+    }
+    
+    return votes>n/2 ? majEle : -1;
+    
+}
---
+++ Striver-SDE-Sheet-Challenge/Recursion/CombinationSum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    void solve(int ind, int target, vector<int>& candidates, vector<int>& comb, vector<vector<int>>& ans){
+        int n = candidates.size();
+        if(ind >= n){
+            if(target == 0) ans.push_back(comb);
+            return;
+        }
+        
+
+        // Take the current candidate;
+        if(candidates[ind] <= target){
+            comb.push_back(candidates[ind]);
+            solve(ind, target-candidates[ind], candidates, comb, ans);
+            comb.pop_back();
+        }
+        //Not Take the current candidate;
+        solve(ind+1, target, candidates, comb, ans);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> comb;
+        solve(0, target, candidates, comb, ans);
+        return ans;
+    }
+};
